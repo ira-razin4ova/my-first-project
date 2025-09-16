4b69c794de947a7c409db598a9219bbd362c69ce
irina_razinkova <ipopova024@gmail.com>
2025-09-16 14:09:09 +0300

    cheng in cod

--- my-first-project/src/Main.java
+++ my-first-project/src/Main.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         // comment this
         System.out.println("Hello and welcome!\nWhats up");
         System.out.println("Hello and welcome!\nWhats up?");
